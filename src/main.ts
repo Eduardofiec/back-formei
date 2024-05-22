0ee58ea45206fe8d5534565f4546f1d415a4338b
edumast <euedu90@gmail.com>
2024-05-22 19:36:48 -0300

    added Cors to main-ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 import mysql2 from "mysql2"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,  { cors: true });
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
