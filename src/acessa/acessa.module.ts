9494c6ea3f8fdeaa0aa87025c5eda2d192199ef8
edumast <euedu90@gmail.com>
2024-06-06 19:29:01 -0300

    deleting the table acessa

--- src/acessa/acessa.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AcessaService } from './acessa.service';
-import { AcessaController } from './acessa.controller';
-
-@Module({
-  controllers: [AcessaController],
-  providers: [AcessaService],
-})
-export class AcessaModule {}
